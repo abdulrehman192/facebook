273f31a7004d3292b1e187770bf2571248951edd
ABDUL REHMAN <mr.abdulrehman.ar@gmail.com>
2021-10-18 17:01:49 +0500

    postcard

--- lib/feeds.dart
+++ lib/feeds.dart
@@ -11,6 +11,7 @@ class Feeds extends StatefulWidget {
 class _FeedsState extends State<Feeds> {
   @override
   Widget build(BuildContext context) {
+
     return Container(
       color: Colors.grey,
       child: SingleChildScrollView(
@@ -21,8 +22,110 @@ class _FeedsState extends State<Feeds> {
             roomCard(),
             SizedBox(height: 8,),
             storyCard(),
+            SizedBox(height: 8,),
+            post(),
+            post(),
           ],
         ),
+      ),
+    );
+  }
+
+  postList()
+  {
+    return ListView.builder(
+        itemCount: 200,
+        itemBuilder: (context, index)
+    {
+      return post();
+    }
+    );
+  }
+
+  Widget post()
+  {
+    return Container(
+      margin: EdgeInsets.only(bottom: 8.0),
+      width: MediaQuery.of(context).size.width,
+      // height: 500,
+      color: Colors.white,
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            leading: CircleAvatar(),
+            title: Text('Username',style: TextStyle(fontWeight: FontWeight.bold),),
+            subtitle: Row(
+              children: <Widget>[
+                Text('7m'),
+                SizedBox(width: 3.0,),
+                Icon(Icons.public,size: 15.0,)
+              ],
+            ),
+            trailing: IconButton(
+              onPressed: (){},
+              icon: Icon(Icons.more_horiz),
+            ),
+          ),
+          Container(
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.symmetric(horizontal: 8.0),
+            child: Text('Post caption text goes here \n Hello how are you'),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            margin: EdgeInsets.symmetric(vertical: 3.0),
+            height: 250,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/2.jpg'),
+                fit: BoxFit.fill
+              )
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.thumb_up,color: Colors.blue,),
+                      Icon(Icons.emoji_emotions,color: Colors.yellow,),
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Row(
+                    children: <Widget>[
+                      Text('30 Comments'),
+                      Text('15 Shares')
+                    ],
+                  ),
+                ),
+
+              ],
+            ),
+          ),
+          Divider(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              postButton(
+                icon: Icon(Icons.thumb_up_outlined),
+                buttonText: 'Like'
+              ),
+              postButton(
+                  icon: Icon(Icons.mode_comment_outlined),
+                  buttonText: 'Comment'
+              ),
+              postButton(
+                  icon: Icon(Icons.share),
+                  buttonText: 'Share'
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
@@ -49,7 +152,8 @@ class _FeedsState extends State<Feeds> {
                       margin: EdgeInsets.symmetric(horizontal: 3.0),
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(12.0),
-                        color: Colors.blue
+                        color: Colors.blue,
+
                       ),
                   );
                 }
@@ -145,7 +249,6 @@ class _FeedsState extends State<Feeds> {
                 color: Colors.grey,
               ),
               postButton(icon: Icon(Icons.video_call_rounded, color: Colors.purple,), buttonText: 'Room'),
-
             ],
           )
         ],
